b1e18a794934ec4b8ef79675511853e22996011a
Wendel Santos <wendel_209@hotmail.com>
2024-02-17 14:43:53 -0300

    Atualização de rota de uma imagem das capas da home Page.

--- src/componentes/Home/capasDepartamentos/images.tsx
+++ src/componentes/Home/capasDepartamentos/images.tsx
@@ -1,6 +1,6 @@
 import Make from './imagesProducts/Maquiagem.png';
 import Linhas from  './imagesProducts/Linhas.png';
-import Ofertas from './imagesProducts/ofertas.png'
+import Ofertas from './imagesProducts/Ofertas.png';
 
 export const capasImages = [
     {id: '1', picture: Ofertas},
