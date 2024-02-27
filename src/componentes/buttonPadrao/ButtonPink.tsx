0b244d6ed8fee40ef767e92c4d8e63646243f720
JeshuaBen <dev.09@pbhub.com.br>
2024-02-27 15:45:59 -0300

    feat: implementing some structural changes

--- src/componentes/buttonPadrao/ButtonPink.tsx
+++ src/componentes/buttonPadrao/ButtonPink.tsx
@@ -1,23 +1,44 @@
-import styles from './ButtonPink.module.css'
+import styles from "./ButtonPink.module.css";
 
 interface button {
-    text: string
-    grande?: boolean
-    medio?: boolean
-    funciton?: void
+  text: string;
+  grande?: boolean;
+  medio?: boolean;
+  funciton?: void;
 }
 
-export default function ButtonPink(props: button){
-    
+export default function ButtonPink(props: button) {
+  const grandeOuPequeno = props.grande
+    ? styles.grande
+    : props.medio
+    ? styles.medio
+    : styles.pink;
 
-    const grandeOuPequeno = props.grande? styles.grande : props.medio? styles.medio : styles.pink;
-    return(
-        <div className={styles.moveButton}>
-            <button className={grandeOuPequeno}>
-                {props.text}
-            </button>
-        </div>
-    )
+  // Uma boa abordagem seria fazer uma função auxiliar que faz o tratamento desse ternario. Nesse caso como ele já passou de apenas uma verificação ai no caso de ser feito uma função auxiliar. Tipo essa:
+
+  /*
+
+      const getSizeClass = (props: ButtonProps) => {
+  const sizeMap = {
+    grande: styles.grande,
+    medio: styles.medio,
+    default: styles.pink,
+  };
+
+  const sizeKey = Object.keys(props).find(key => sizeMap[key]);
+
+  return sizeMap[sizeKey] || styles.pink;
+};
+    
+  */
+
+  // Ai vc passaria para o grandeOuPequeno dessa forma: const grandeOuPequeno = getSizeClass(props);
+
+  return (
+    <div className={styles.moveButton}>
+      <button className={grandeOuPequeno}>{props.text}</button>
+    </div>
+  );
 }
 /*button sendo compartilhado em diferentes partes da aplicações com parametros para dizer se ele
 vai ser grande, medio ou pequeno*/
